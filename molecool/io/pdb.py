0cd4d2e9c353cf9d83d257618adb578a610506da
Stephen Keegan <swtkeegan@gmail.com>
2020-01-25 15:20:49 -0500

    added docstrings to open_pdb function

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -6,6 +6,27 @@ import numpy as np
 
 
 def open_pdb(file_location):
+    """
+    Open and read coordinates and symbols from a pdb file (pdb = protein database)
+
+    The pdb file must specify the atom elements in the last column, and follow the conventions outlines in the PDB format specification
+
+    Parameters
+    ----------
+ 
+    file_location : str
+       The location of the pdb file to read in
+
+    Returns
+    -------
+    coords : np.ndarray
+	The coordinates from the pdb file.
+    symbols : list
+       The atomic symbols from the pdb file
+
+    """
+
+
     with open(file_location) as f:
         data = f.readlines()
     
